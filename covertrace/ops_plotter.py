0a930398839a97ad39031fec2f9e30b90a5381bd
braysia <kudo@DN0a22b924.SUNet>
2016-07-20 15:07:27 -0700

    ops_plotter bug fixed

--- covertrace/ops_plotter.py
+++ covertrace/ops_plotter.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-
+import numpy as np
 
 def plot_all(arr, ax=None, **kwargs):
     pd.DataFrame(arr.T).plot(legend=False, ax=ax, **kwargs)
@@ -11,7 +11,7 @@ def plot_heatmap(arr, ax=None, **kwargs):
 
 
 def plot_tsplot(arr, ax=None, **kwargs):
-    sns.tsplot(arr, ax=ax)
+    sns.tsplot(arr, estimator=np.nanmean, ax=ax, **kwargs)
 
 
 def plot_histogram_pdstats(arr, ax, pd_func_name='mean', **keys):
